d495aa6b58c434d04832353a187eafdd26a01d44
maxwilliams361 <maxwell.williams@codenation.org>
2019-10-31 18:27:50 +0000

    Randomized output, added conditionals

--- js/script.js
+++ js/script.js
@@ -11,4 +11,20 @@
 $("#shoot").click(function(){
     let pplChoice=$("#input").val();
 $("#userChoice").text(pplChoice);
+    let randomNumber=Math.random();
+if (randomNumber<=.3333333){
+    $("#computerChoice").text("Rock");}
+else if (randomNumber>.3333333 && randomNumber<=.6666666){
+    $("#computerChoice").text("Paper");
+}
+else if (randomNumber>.6666666){
+    $("#computerChoice").text("Scissors");
+}
+if(pplChoice===rock && randomNumber<=.3333333){
+    $("#result").text("Draw!");
+}
+if(pplChoice===rock&&randomNumber>.3333333&&randomNumber<=.6666666){
+    $("#result").text("Computer wins!")
+}
+
 });
